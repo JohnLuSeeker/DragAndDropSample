fee016e656eee4b2d312369a26cd36e3ad337070
johnlu <johnlu@seekrtech.com>
2021-12-14 17:13:21 +0800

    Add `ItemTouchHelper` into `RecyclerViewAdapter`

--- app/src/main/java/com/nogotech/draganddropsample/RecyclerViewAdapter.kt
+++ app/src/main/java/com/nogotech/draganddropsample/RecyclerViewAdapter.kt
@@ -3,6 +3,9 @@ package com.nogotech.draganddropsample
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.DOWN
+import androidx.recyclerview.widget.ItemTouchHelper.UP
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.nogotech.draganddropsample.databinding.ItemStringBinding
@@ -12,17 +15,60 @@ import com.nogotech.draganddropsample.databinding.ItemStringBinding
  */
 class RecyclerViewAdapter(private val clickListener: Listener) :
     ListAdapter<String, RecyclerViewAdapter.StringViewHolder>(
-        object : DiffUtil.ItemCallback<String>()
-        {
+        object : DiffUtil.ItemCallback<String>() {
             override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
-                return  oldItem.contentEquals(newItem)
+                return oldItem.contentEquals(newItem)
             }
 
             override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
-                return  oldItem.contentEquals(newItem)
+                return oldItem.contentEquals(newItem)
             }
         }
     ) {
+
+    private val itemTouchHelper by lazy {
+        ItemTouchHelper(
+            object : ItemTouchHelper.SimpleCallback(
+                UP or DOWN, 0
+            ) {
+                /**
+                 * onMove lets you check if an item has been moved
+                 * from its position either up or down
+                 */
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    TODO("Not yet implemented")
+                }
+
+                /**
+                 * onSwiped tells you when an item is swiped
+                 * left or right from its position ( swipe to delete)
+                 */
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    TODO("Not yet implemented")
+                }
+
+                /**
+                 * When an item changes its location that is currently selected
+                 * this function is called
+                 */
+                override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+                    super.onSelectedChanged(viewHolder, actionState)
+
+                }
+
+                /**
+                 * When we stop dragging , swiping or moving an item this function is called
+                 */
+                override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+                    super.clearView(recyclerView, viewHolder)
+                }
+
+            })
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StringViewHolder {
         return StringViewHolder.from(parent)
@@ -48,7 +94,8 @@ class RecyclerViewAdapter(private val clickListener: Listener) :
 
         companion object {
             fun from(parent: ViewGroup): StringViewHolder {
-                val binding = ItemStringBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                val binding =
+                    ItemStringBinding.inflate(LayoutInflater.from(parent.context), parent, false)
                 return StringViewHolder(binding)
             }
         }
